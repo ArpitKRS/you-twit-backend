be5bda553dc8795f130459abe07c3067d294f40c
Arpit Kumar Singh <singharpit302@gmail.com>
2025-02-13 23:01:11 +0530

    modification user routes added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {ApiResponse} from '../utils/ApiResponse.js'
 import User from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
 
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -278,6 +279,49 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .json( new ApiResponse(200, channel[0], "User channel fetched successully!") )
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: { _id: new mongoose.Types.ObjectId(req.user?._id)}
+        },
+        {
+            $lookup: {
+                form: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchedHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: { fullname: 1, username: 1, avatar: 1 }
+                                }
+                            ]
+                        }
+                    },
+                    // For fetching the first value of the result Array (for frontend ease)
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"))
+})
+
 export {
     registerUser, 
     loginUser, 
@@ -288,5 +332,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
