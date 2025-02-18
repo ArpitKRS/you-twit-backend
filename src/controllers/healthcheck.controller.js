23e5d6aa1b5ba131d24e339ea73ea307cbd1e126
Arpit Kumar Singh <singharpit302@gmail.com>
2025-02-18 23:32:47 +0530

    Added healthcheck and like controllers

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -3,8 +3,14 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
-const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+const healthcheck = asyncHandler(async (_, res) => {
+    try {
+        return res
+        .status(200)
+        .json( new ApiResponse(200, {status: "OK"}, "Server is already running...") )
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong during Healthcheck")
+    }
 })
 
 export { healthcheck }
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,23 +6,82 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+    const userId = req.user?._id
+    if (!isValidObjectId(videoId)) throw new ApiError(400, "Invalid videoId passed");
+    if (!userId) throw new ApiError(400, "User needs to login to like/dislike the video");
+
+    const existingLike = await Like.findOne({ video: videoId, likedBy: userId })
+    if (existingLike) {
+        // Video already liked, find and delete
+        await Like.findByIdAndDelete(existingLike?._id)
+        return res
+        .status(200)
+        .json( new ApiResponse(200, existingLike, "Video unliked successfully!") );
+    }
+
+    // If no like exists, create new like
+    const likeVideo = await Like.create({ video: videoId, likedBy: userId })
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, likeVideo, "Video liked successfully!"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
-    //TODO: toggle like on comment
+    const userId = req.user?._id
+    if (!isValidObjectId(commentId)) throw new ApiError(400, "Invalid comment ID passed");
+    if (!userId) throw new ApiError(400, "User needs to login to like/dislike the comment");
 
+    const existingLike = await Like.findOne({ comment: commentId, likedBy: userId })
+    if (existingLike) {
+        // Comment already liked, find and delete
+        await Like.findByIdAndDelete(existingLike?._id)
+        return res
+        .status(200)
+        .json( new ApiResponse(200, existingLike, "Comment unliked successfully!") );
+    }
+
+    // If not already liked, create the liked comment
+    const likeComment = await Like.create({ comment: commentId, likedBy: userId })
+    return res
+    .status(201)
+    .json(new ApiResponse(201, likeComment, "Comment liked successfully!"))
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
-    //TODO: toggle like on tweet
+    const userId = req.user?._id
+    if (!isValidObjectId(tweetId)) throw new ApiError(400, "Invalid tweet ID passed");
+    if (!userId) throw new ApiError(400, "User needs to login to like/dislike the tweet");
+
+    const existingTweet = await Like.findOne({tweet: tweetId, likedBy: userId})
+    if (existingTweet) {
+        // Tweet already liked, find the tweet and delete
+        await Like.findByIdAndDelete(existingTweet?._id)
+        return res
+        .status(200)
+        .json( new ApiResponse(200, existingTweet, "Tweet disliked successfully") )
+    }
+
+    // If not liked, create a liked tweet
+    const likeTweet = await Like.create({ tweet: tweetId, likedBy: userId })
+
+    return res
+    .status(201)
+    .json( new ApiResponse(201, likeTweet, "Tweet Liked successfully!") )
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const userId = req.user?._id
+    if (!userId) throw new ApiError(400, "User needs to login to see their liked videos");
+
+    const likedVideos = await Like.find({ likedBy: userId, video: { $exists: true }}).populate("video", "_id title url")
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, likedVideos, "Liked Videos fetched successfully") )
 })
 
 export {
